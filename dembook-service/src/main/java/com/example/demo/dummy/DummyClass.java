3e1fe19dc1ff16d85605f4c281ad02e8e11ff584
deeksha <deekshabhat712@gmail.com>
2021-10-07 15:46:59 +0530

    dummy message

---
+++ dembook-service/src/main/java/com/example/demo/dummy/DummyClass.java
@@ -0,0 +1,17 @@
+package com.example.demo.dummy;
+
+public class DummyClass {
+	
+	private int dummyId;
+
+	public DummyClass(int dummyId) {
+		super();
+		this.dummyId = dummyId;
+	}
+
+	@Override
+	public String toString() {
+		return "DummyClass [dummyId=" + dummyId + "]";
+	}
+	
+}
