47dbed2bf477fe00bda6636d54731aa0585d5feb
deeksha <deekshabhat712@gmail.com>
2021-10-07 14:51:54 +0530

    REQ-001 Created a sample memeber class

---
+++ git-push-member-example/src/com/example/demo/entity/Application.java
@@ -0,0 +1,14 @@
+package com.example.demo.entity;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Member m1=new Member(101,"Meena");
+		//Member m2=new Member(102,"Sona");
+		System.out.println(m1.toString());
+		
+	}
+
+}
